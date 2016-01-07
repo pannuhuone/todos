285465bb6ab230a482f4ef43d7e0b291d771b80e
pannuhuone <tao@iki.fi>
2016-01-07 19:12:21 +0200

    End of "Getting started"

--- todos.js
+++ todos.js
@@ -1,1 +1,38 @@
+if(Meteor.isClient){
+  Template.todos.helpers({
+    'todo': function(){
+      return Todos.find({}, {sort: {createdAt: -1}});
+    }
+  });
+
+    Template.addTodo.events({
+        'submit form': function(event){
+            event.preventDefault();
+            var todoName = $('[name="todoName"]').val();
+            Todos.insert({
+               name: todoName,
+                completed: false,
+                createdAt: new Date()
+            });
+            $('[name="todoName"]').val('');
+        }
+    });
+    Template.todoItem.events({
+        'click .delete-todo': function(event){
+            event.preventDefault();
+            var documentId = this._id;
+            var confirm = window.confirm("Delete this task?");
+            if(confirm) {
+                Todos.remove({_id: documentId});
+            }
+        }
+    });
+}
+
+
+if(Meteor.isServer){
+
+}
+
+Todos = new Mongo.Collection('todos');
 
