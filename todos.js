5fabdeac332457aac9596bf35cc555b90292ed9c
pannuhuone <tao@iki.fi>
2016-01-07 23:41:07 +0200

    End of "Managing Todos"

--- todos.js
+++ todos.js
@@ -17,6 +17,7 @@ if(Meteor.isClient){
             $('[name="todoName"]').val('');
         }
     });
+
     Template.todoItem.events({
         'click .delete-todo': function(event){
             event.preventDefault();
@@ -25,6 +26,44 @@ if(Meteor.isClient){
             if(confirm) {
                 Todos.remove({_id: documentId});
             }
+        },
+        'keyup [name=todoItem]': function(event) {
+            if (event.which == 13 || event.which == 27) {
+                $(event.target).blur();
+            } else {
+                var documentId = this._id;
+                var todoItem = $(event.target).val();
+                Todos.update({_id: documentId}, {$set: {name: todoItem}});
+            }
+        },
+        'change [type=checkbox]': function(){
+            var documentId = this._id;
+            var isCompleted = this.completed;
+            if(isCompleted){
+                Todos.update({ _id: documentId }, {$set: { completed: false }});
+            } else {
+                Todos.update({ _id: documentId }, {$set: { completed: true }});
+            }
+        }
+    });
+
+    Template.todoItem.helpers({
+        'checked': function() {
+            var isCompleted = this.completed;
+            if(isCompleted){
+                return "checked";
+            } else {
+                return "";
+            }
+        }
+    });
+
+    Template.todosCount.helpers({
+        'totalTodos': function(){
+            return Todos.find().count();
+        },
+        'completedTodos': function(){
+            return Todos.find({ completed: true }).count();
         }
     });
 }
